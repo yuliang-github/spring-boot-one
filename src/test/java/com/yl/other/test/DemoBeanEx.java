13893e51d2953a82a206e7686fe34a19a0d15b44
alex <824187670@qq.com>
2018-09-05 16:25:48 +0800

    debug demo

---
+++ src/test/java/com/yl/other/test/DemoBeanEx.java
@@ -0,0 +1,45 @@
+package com.yl.other.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Alex
+ * @since 2018/9/5 11:14
+ */
+public class DemoBeanEx {
+
+    public static enum Type {
+        MAN(1,"男"),WOMAN(2,"女"),UNKNOW(3,"未知");
+
+        private static Map<Integer,Type> cache = null;
+
+        static {
+            cache = new HashMap<>();
+            Type[] types = Type.values();
+            for (Type type : types) {
+                cache.put(type.code, type);
+            }
+        }
+
+        private int code;
+        private String des;
+        private Type(int code,String des){
+            this.code = code;
+            this.des = des;
+        }
+
+        public String getDes(){
+            return this.des;
+        }
+
+        public static Type get(int code){
+            return cache.get(code);
+        }
+    }
+
+    public static void syse(){
+        System.err.println("调试技巧");
+    }
+
+}
---
+++ src/test/java/com/yl/other/test/OtherTest.java
@@ -0,0 +1,28 @@
+package com.yl.other.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Alex
+ * @since 2018/9/5 11:14
+ */
+public class OtherTest {
+
+    public static void main(String[] args) {
+
+        String des = DemoBeanEx.Type.get(1).getDes();
+
+        System.err.println(des);
+
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        System.err.println(list);
+
+        DemoBeanEx.syse();
+
+        System.err.println(list);
+
+    }
+
+}
