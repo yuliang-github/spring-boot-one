8419c3c5ae8d07615420c1c76caf50adc199a0c0
alex <824187670@qq.com>
2018-09-21 11:05:49 +0800

    interceptor

--- src/main/java/com/yl/common/demo/config/EventListenner.java
+++ src/main/java/com/yl/common/demo/config/EventListenner.java
@@ -3,6 +3,7 @@ package com.yl.common.demo.config;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.support.ServletRequestHandledEvent;
 
 /**
  * @author Alex
@@ -13,6 +14,11 @@ public class EventListenner implements ApplicationListener<ApplicationEvent> {
 
     @Override
     public void onApplicationEvent(ApplicationEvent event) {
-        System.err.println("监听spring事件:" + event.getSource());
+//        System.err.println("监听spring事件:" + event.getSource());
+        if(event instanceof ServletRequestHandledEvent){
+            ServletRequestHandledEvent servletEvent = (ServletRequestHandledEvent) event;
+            String requestUrl = servletEvent.getRequestUrl();
+            System.err.println("请求URL:" + requestUrl);
+        }
     }
 }
--- src/main/java/com/yl/springboot/config/MvcConfig.java
+++ src/main/java/com/yl/springboot/config/MvcConfig.java
@@ -6,10 +6,15 @@ import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
@@ -41,4 +46,35 @@ public class MvcConfig implements WebMvcConfigurer {
     }
 
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        HandlerInterceptor interceptor = new HandlerInterceptor() {
+            /**
+             * 在请求之前拦截
+             */
+            @Override
+            public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+                System.err.println("请求之前url:" + request.getRequestURI());
+                return true;
+            }
+
+            /**
+             * 在请求之后,视图渲染之前拦截
+             */
+            @Override
+            public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+                System.err.println("请求之后url:" + request.getRequestURI());
+            }
+
+            /**
+             * 在请求全部完成之后拦截
+             */
+            @Override
+            public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+                System.err.println("请求完成url:" + request.getRequestURI());
+            }
+        };
+        registry.addInterceptor(interceptor).addPathPatterns("/*");
+    }
 }
