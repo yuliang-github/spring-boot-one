88aa0eb1446f65e2e484c109795bb68428707711
alex <82418767002qq.com>
2019-07-07 21:50:44 +0800

    begin dubbo analysis

---
+++ src/main/java/com/yl/common/controller/ratelimit/RateLimitController.java
@@ -0,0 +1,20 @@
+package com.yl.common.controller.ratelimit;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author yu.alex
+ * @date 2019/7/1 21:00
+ * @des
+ */
+@Controller
+public class RateLimitController {
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
---
+++ src/main/java/com/yl/springboot/config/DubboConfig.java
@@ -0,0 +1,18 @@
+package com.yl.springboot.config;
+
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author yu.alex
+ * @date 2019/7/6 9:50
+ * @des
+ */
+@Configuration
+// 使用SrpingBoot整合Dubbo
+@EnableDubbo
+public class DubboConfig {
+
+
+
+}
